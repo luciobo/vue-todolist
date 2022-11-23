9f533adcefd6cef1eb8f71f1bf359dcbd4ffef67
luciobo <luciobo@icloud.com>
2022-11-23 15:02:05 +0100

    commit iniziale

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+// Dall'oggetto globale Vue, estrai in una variabile la funzione "createApp"
+const { createApp } = Vue;
+
+// invochiamo la funzione passando come argomento un oggetto
+
+// crea l'istanza di Vue. Sull'istanza invochiamo la funzione
+// mount. Questa si aspetta come un argomento un selettore html valido
+createApp({
+    // data: function () {}
+    data() {
+        // la funzione data deve SEMPRE ritornare un oggetto
+        return {};
+    },
+}).mount("#app")
